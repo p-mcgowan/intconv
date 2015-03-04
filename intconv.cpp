24169d05d78b55c7076c0dfa810aaa26123c56b0
p-mcgowan <mickeymcgowan@shaw.ca>
2015-03-03 23:19:19 -0800

    now takes stdin

--- intconv.cpp
+++ intconv.cpp
@@ -1,101 +1,129 @@
 #include "intconv.h"
 
 int main (int argc, char *argv[]) {
-	if (argc != 3 && argc != 4) {
-		std::cout <<
-		"Usage: conv OPTIONS INPUT [OUTPUT]" << std::endl <<
-		"Converts INPUT from base OPTIONS to other bases." << std::endl <<
-		"Options:" << std::endl <<
-		"  -b    binary" << std::endl <<
-		"  -o    octal" << std::endl <<
-		"  -d    decimal" << std::endl <<
-		"  -h    hex" << std::endl <<
-		"  -c    ASCII char" << std::endl <<
-		"If OUTPUT is specified, prints only that base." << std::endl;
-		return 1;
-	}
-	unsigned num;
-	if (!strcmp(argv[1], "-b")) {
-		if (!isValidBinary(argv[2])) {
-			errm("Invalid binary: " << argv[2]);
-		}
-		num = base2d(argv[2], BIN);
-	}
-	else if (!strcmp(argv[1], "-d")) {
-		if (!isValidDecimal(argv[2])) {
-			errm("Invalid decimal: " << argv[2]);
-		}
-		num = atoi(argv[2]);
-	}
-	else if (!strcmp(argv[1], "-c")) {
-		if (!isValidChar(argv[2])) {
-			errm("Invalid char: " << argv[2]);
-		}
-		num = (int)argv[2][0];
-	}
-	else if (!strcmp(argv[1], "-h")) {
-		if (!isValidHex(argv[2])) {
-			errm("Invalid hex: " << argv[2]);
-		}
-		num = base2d(argv[2], HEX);
-	}
-	else if (!strcmp(argv[1], "-o")) {
-		if (!isValidOctal(argv[2])) {
-			errm("Invalid octal: " << argv[2]);
-		}
-		num = base2d(argv[2], OCT);
-	}
-	else {
-		errm("Invalid format");
-	}
-	if (argc == 3) {
-		logd("Binary  : " << d2base(num, BIN));
-		logd("Octal   : " << d2base(num, OCT));
-		logd("Decimal : " << num);
-		logd("Hex     : " << d2base(num, HEX));
-		logd("ASCII   : " << (char)num);
-		return 0;
-	}
-	if (!strcmp(argv[3], "-h"))
-		logd(d2base(num, HEX));
-	if (!strcmp(argv[3], "-b"))
-		logd(d2base(num, BIN));
-	if (!strcmp(argv[3], "-d"))
-		logd(num);
-	if (!strcmp(argv[3], "-o"))
-		logd(d2base(num, OCT));
-	if (!strcmp(argv[3], "-c"))
-		logd((char)num);
-	return 0;
+  char base_in, base_out;
+  if (!isatty(fileno(stdin))) {
+    if (argc != 2 && argc != 3) {
+      usage();
+      return -1;
+    }
+    base_in = (argv[1][0] == '-')? argv[1][1] : '?';
+    base_out = (argc == 3)? (argv[2][0] == '-')? argv[2][1] : '?' : '?';
+    char buffer[BUFFER_SIZE];
+
+    while (fgets(buffer, BUFFER_SIZE, stdin)) {
+      buffer[strlen(buffer) - 1] = (buffer[strlen(buffer) - 1] == '\n')? '\0' : buffer[strlen(buffer) - 1];
+      formatted_output(base_in, base_out, buffer, argc + 1);
+    }
+    return 0;
+  } else if (argc != 3 && argc != 4) {
+    usage();
+    return -1;
+  }
+
+  base_in = (argv[1][0] == '-')? argv[1][1] : '?';
+  base_out = (argc == 4)? (argv[2][0] == '-')? argv[2][1] : '?' : '?';
+  formatted_output(base_in, base_out, argv[argc - 1], argc);
+  return 0;
+}
+
+void formatted_output(char base_in, char base_out, char *input, int args) {
+  unsigned num;
+  switch (base_in) {
+    case 'b':
+      if (!isValidBinary(input)) {
+        errm("Invalid binary");
+      }
+      num = base2d(input, BIN);
+      break;
+    case 'd':
+      if (!isValidDecimal(input)) {
+        errm("Invalid decimal");
+      }
+      num = std::stoi(input);
+      break;
+    case 'c':
+      if (!isValidChar(input)) {
+        errm("Invalid char");
+      }
+      num = (int)input[0];
+      break;
+    case 'h':
+      if (!isValidHex(input)) {
+        errm("Invalid hex");
+      }
+      num = base2d(input, HEX);
+      break;
+    case 'o':
+      if (!isValidOctal(input)) {
+        errm("Invalid octal");
+      }
+      num = base2d(input, OCT);
+      break;
+    default: errm("Invalid format");
+  }
+  if (args == 3) {
+    out_all(num);
+    return;
+  }
+  switch (base_out) {
+    case 'b': logd(d2base(num, BIN)); break;
+    case 'd': logd(num); break;
+    case 'c': logd((char)num); break;
+    case 'h': logd(d2base(num, HEX)); break;
+    case 'o': logd(d2base(num, OCT)); break;
+    default: errm("Invalid format");
+  }
+}
+
+void out_all(int num) {
+  logd("Binary  : " << d2base(num, BIN));
+  logd("Octal   : " << d2base(num, OCT));
+  logd("Decimal : " << num);
+  logd("Hex     : " << d2base(num, HEX));
+  logd("ASCII   : " << (char)num);
+}
+
+void usage() {
+  std::cout <<
+    "Usage: conv INPUT_BASE [OUTPUT_BASE] INPUT" << std::endl <<
+    "Converts INPUT from base INPUT_BASE to other bases." << std::endl <<
+    "Base options:" << std::endl <<
+    "  -b    binary" << std::endl <<
+    "  -o    octal" << std::endl <<
+    "  -d    decimal" << std::endl <<
+    "  -h    hex" << std::endl <<
+    "  -c    ASCII char" << std::endl <<
+    "If OUTPUT is specified, prints only that base." << std::endl;
 }
 
 unsigned base2d(std::string n, int b) {
-	unsigned d = 0;
-	int i = 0;
-	for (int c = n.size() - 1; c >= 0; c--) {
-		if (toupper(n[c]) > '9')
-			d += (int)(10 + toupper(n[c])-'A')*pow(b,i++);
-		else
-			d += (int)(n[c] - '0')*pow(b,i++);
-	}
-	return d;
+  unsigned d = 0;
+  int i = 0;
+  for (int c = n.size() - 1; c >= 0; c--) {
+    if (toupper(n[c]) > '9')
+      d += (int)(10 + toupper(n[c])-'A')*pow(b,i++);
+    else
+      d += (int)(n[c] - '0')*pow(b,i++);
+  }
+  return d;
 }
 
 std::string d2base(unsigned d, int b) {
-	if (d == 0)
-		return "0";
-	std::string base;
-	int q = 0, r = 0;
-	char v;
-	while (d > 0) {
-		q = d / b;
-		r = (unsigned)d % b;
-		if (r > 9)
-			v = 'A' + r - 10;
-		else 
-			v = '0' + r;
-		base = v + base;
-		d = q;
-	}
-	return base;
+  if (d == 0)
+    return "0";
+  std::string base;
+  int q = 0, r = 0;
+  char v;
+  while (d > 0) {
+    q = d / b;
+    r = (unsigned)d % b;
+    if (r > 9)
+      v = 'A' + r - 10;
+    else
+      v = '0' + r;
+    base = v + base;
+    d = q;
+  }
+  return base;
 }
--- intconv.h
+++ intconv.h
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <cmath>
 #include <string>
-#include <cstring>
+#include <cstring>  //???
 #include <stdlib.h>
+#include <unistd.h>
 
 #define HEX 16
 #define OCT 8
 #define BIN 2
 
+#define BUFFER_SIZE 128
+
 #define logd(msg) std::cout << msg << std::endl;
-#define errm(msg) logd(msg); return -1;
+#define errm(msg) logd(msg); exit(-1);
+
+// Help message
+void usage();
+
+// Output formatting
+void formatted_output(char base_in, char base_out, char *input, int argc);
+void out_all(int num);
 
 // Conversions
 std::string d2base(unsigned d, int b);
@@ -17,37 +27,41 @@ unsigned base2d(std::string n, int b);
 
 // Valid input format
 bool isValidBinary(std::string s) {
-	for (size_t c = 0; c < s.size(); c++) {
-		if (s[c] != '1' && s[c] != '0')
-			return false;
-	}
-	return true;
+  for (size_t c = 0; c < s.size(); c++) {
+    if (s[c] != '1' && s[c] != '0')
+      return false;
+  }
+  return true;
 }
+
 bool isValidDecimal(std::string s) {
-	for (size_t c = 0; c < s.size(); c++) {
-		if (s[c] < '0' || s[c] > '9')
-			return false;
-	}
-	return true;
+  for (size_t c = 0; c < s.size(); c++) {
+    if (s[c] < '0' || s[c] > '9')
+      return false;
+  }
+  return true;
 }
+
 bool isValidChar(std::string s) {
-	if (s.size() != 1)
-		return false;
-	if ((int)s[0] < 0 || (int)s[0] > 255)
-		return false;
-	return true;
+  if (s.size() != 1)
+    return false;
+  if ((int)s[0] < 0 || (int)s[0] > 255)
+    return false;
+  return true;
 }
-bool isValidOctal(std::string s) {
-	for (size_t c = 0; c < s.size(); c++) {
-		if (s[c] < '0' || s[c] > '7')
-			return false;
-	}
-	return true;
+bool
+ isValidOctal(std::string s) {
+  for (size_t c = 0; c < s.size(); c++) {
+    if (s[c] < '0' || s[c] > '7')
+      return false;
+  }
+  return true;
 }
+
 bool isValidHex(std::string s) {
-	for (size_t c = 0; c < s.size(); c++) {
-		if (!((tolower(s[c]) >= 'a' && tolower(s[c]) <= 'f') ||  (s[c] >= '0' && s[c] <= '9')))
-			return false;
-	}
-	return true;
+  for (size_t c = 0; c < s.size(); c++) {
+    if (!((tolower(s[c]) >= 'a' && tolower(s[c]) <= 'f') ||  (s[c] >= '0' && s[c] <= '9')))
+      return false;
+  }
+  return true;
 }
